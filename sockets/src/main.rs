33ba6d4bce47ea73707c18b85605fe819755cfc7
Albert <albert.lopez@attackiq.com>
2017-01-09 01:50:22 +0100

    sockets: Convert tcp client to struct+impl
    Many approaches have been tried to achieve the solution reached.
    
    The problem was that connect and send_message functions should be
    independent. That means that the user should be able to connect
    once and send multiple messages.
    
    This approach had the problem that the tcp stream variable needed
    to be shared among the connect and send_message methods, so it
    needed to be part of the struct.
    The fact that Rust does not have default values for struct members
    made that the user had to initialize the tcp stream in order to
    instantiate the struct. This was a problem because I wanted to
    abstract the tcp stream from the user, so she only needed to know
    host and port. To fix that issue many things were tried until this
    final solution was reached.
    
    The solutions were:
    
      1. Use TcpBuilder from net2 crate
    
    This solution tried to give a default (unitialized) value to the
    tcp stream given that we can change the tcp stream variable and
    use TcpBuilder without starting a connection yet, just using the
    associated function TcpBuilder::new_v4() and pass it when
    instantiating the client struct.
    
    This was still a problem because TcpBuilder sucks a lot and it does
    not allow you to extract the TcpStream from the TcpBuilder multiple
    times (convert from TcpBuilder to TcpStream). So the problem was that
    the struct had the a variable of type TcpBuilder, but when the
    TcpBuilder connect() method was called, the TcpStream variable inside
    the TcpBuilder was extracted (and left) and when the send_message
    method was called and self.tcp_builder.to_tcp_stream() was called, it
    failed because the tcp stream inside the tcp builder was already
    consumed by the connect.
    
    This problem left us with the same issue we had before. We need
    a tcp stream that needs to be shared among impl methods, but we only
    get its value after instantiating the TCP client.
    
    TcpBuilder from net2 crate allowed us to give a default (empty) value
    to the tcp variable in the struct, but it could not be converted to
    a tcp stream and passed among methods.
    
      2. Use builder pattern
    
    This solution tried to hide the tcp stream variable from the user by
    giving a ClientBuilder struct to the user that internally instantiates
    the Client struct.
    
    It did not work because when the builder was instantiated, we could
    pass only host and port, but eventually, before the finalize() - when
    the Client struct is instantiated - is called, the TcpStream must have
    been passed. This left us in the same position as before, just delaying
    a little bit the use of TcpStream and overengineering the code.
    
      3. Use an associated function
    
    The final solution fixed the issue by joining the struct instantiation
    and the connection to the server from the client. This solution could
    have been implemented using the builder pattern, but the use of the
    builder pattern was not necessary.
    
    Using an associate function - new() - to the Client struct was an
    intermediate point between having complete separation between struct
    methods. There is some dependency, given that new() calls connect(),
    but the user can expect that to happen in a socket library. Furthermore,
    the associated function new() could be renamed to connect() to make
    things clearer.
    
    In this case, the associate function instantiates the Client struct
    while calling connect, so the tcp stream is created and associated to
    the struct variable that will be shared among all struct methods.

--- sockets/src/main.rs
+++ sockets/src/main.rs
@@ -1,14 +1,32 @@
+use net::client::TCPClient;
 use std::time::Duration;
 use std::thread;
 
 mod net;
 
 fn main() {
+    start_server();
+    thread::sleep(Duration::from_secs(2));
+    let client = start_client();
+    send_messages(&client);
+    thread::sleep(Duration::from_secs(20));
+}
+
+fn start_server() {
     thread::spawn(move || {
         println!("Setting up the server...");
         net::server::listen_for_connections();
     });
-    thread::sleep(Duration::from_secs(2));
-    net::client::connect();
-    thread::sleep(Duration::from_secs(20));
+}
+
+fn start_client() -> net::client::TCPClient {
+    match TCPClient::new("127.0.0.1", "40002") {
+        Ok(client) => client,
+        Err(e) => panic!("client -> Error connecting to server. Error: {}", e.to_string())
+    }
+}
+
+fn send_messages(client: &net::client::TCPClient) {
+    client.send_message("Hi there!");
+    client.send_message("How are you?");
 }
--- sockets/src/net/client.rs
+++ sockets/src/net/client.rs
@@ -1,20 +1,39 @@
-use std::net::TcpStream;
 use std::io::BufWriter;
 use std::io::Write;
+use std::io::Error;
+use std::net::TcpStream;    
 
-
-fn send_messages(stream: &TcpStream) {
-    println!("client -> Sending messages");
-    let mut buf_socket_writer = BufWriter::new(stream);
-    match buf_socket_writer.write(b"Hello!") {
-        Ok(_) => {},
-        Err(e) => println!("Message could not be sent to server. Error: {:?}", e.to_string()),
-    }
+pub struct TCPClient {
+    pub tcp_stream: TcpStream
 }
 
-pub fn connect() {
-    match TcpStream::connect("127.0.0.1:40002") {
-        Ok(stream) => send_messages(&stream),
-        Err(e) => println!("client -> Could not connect to server. Error: {:?}", e.to_string()),
-    } 
+impl TCPClient {
+
+    pub fn new(host: &str, port: &str) -> Result<TCPClient, Error> {
+        match TCPClient::connect(host, port) {
+            Ok(tcp_stream) => {
+                Ok(
+                    TCPClient {tcp_stream: tcp_stream}
+                  )
+            },
+            Err(e) => Result::Err(e)
+        }
+    }
+
+    pub fn send_message(&self, msg: &str) {
+        println!("client -> Sending message: {}", msg);
+        let mut buf_socket_writer = BufWriter::new(&self.tcp_stream);
+        match buf_socket_writer.write(msg.as_bytes()) {
+            Ok(_) => {},
+            Err(e) => println!("Message could not be sent to server. Error: {:?}", e.to_string()),
+        }
+    }
+
+    fn connect(host: &str, port: &str) -> Result<TcpStream, Error> {
+        let uri = format!("{}:{}", host, port);
+        match TcpStream::connect(uri.as_str()) {
+            Ok(tcp_stream) => Ok(tcp_stream),
+            Err(e) => Result::Err(e),
+        } 
+    }
 }
